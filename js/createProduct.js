44cb430d295b859a98d7c7e58aec8ab8475fe841
Rafael Ederli <rafaederli@gmail.com>
2023-01-27 20:20:05 -0300

    createProduct module update

---
+++ js/createProduct.js
@@ -0,0 +1,51 @@
+const createProduct = (imageSrc, productName, productPrice) => {
+    let product = document.createElement("div");
+    product.classList.add("products__item");
+
+    let image = document.createElement("img");
+    image.setAttribute("src", imageSrc);
+    image.setAttribute("alt", productName);
+    image.classList.add("products__item--image");
+    product.appendChild(image);
+    
+    let description = document.createElement("p");
+    description.classList.add("products__item--description");
+    description.innerHTML = productName;
+    product.appendChild(description);
+
+    let price = document.createElement("p");
+    price.classList.add("products__item--price");
+    price.innerHTML = `$ ${productPrice.toFixed(2)}`;
+    product.appendChild(price);
+
+    let counter = document.createElement("div");
+    counter.classList.add("products__item--counter");
+
+    let subtractButton = document.createElement("button");
+    subtractButton.innerHTML = "-";
+    counter.appendChild(subtractButton);
+
+    let display = document.createElement("input");
+    display.setAttribute("type", "text");
+    display.setAttribute("name", "");
+    display.setAttribute("id", "");
+    display.setAttribute("placeholder", "0");
+    display.setAttribute("disabled", "");
+    counter.appendChild(display);
+
+    let addButton = document.createElement("button");
+    addButton.innerHTML = "+";
+    counter.appendChild(addButton);
+    
+    product.appendChild(counter);
+    
+    let cartButton = document.createElement("button");
+    cartButton.classList.add("products__item--button");
+    cartButton.innerHTML = "Adicionar ao carrinho";
+    product.appendChild(cartButton);
+
+    let products = document.querySelector("[data-products]");
+    products.appendChild(product);
+}
+
+export default createProduct;
---
+++ js/index.js
@@ -0,0 +1,3 @@
+import createProduct from "./createProduct.js";
+
+createProduct("img/products/pexels-andre-william-3010771.jpg", "Conjunto Chá", 120);
