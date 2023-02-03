95dd6081da81c6a450247a7816d46b8fcdab6517
Rafael Ederli <rafaederli@gmail.com>
2023-02-03 17:30:20 -0300

    style update

--- js/db.js
+++ js/db.js
@@ -3,19 +3,19 @@ const db = [
         "source": "./img/products/dish/callum-shaw-Vrztewqmi4Q-unsplash.jpg",
         "name": "Prato Branco",
         "price": 85.5,
-        "home-page": true
+        "home-page": false
     },
     {
         "source": "./img/products/dish/kasia-sikorska-fBoijWnTNwo-unsplash.jpg",
         "name": "Prato Leve",
         "price": 86,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/dish/pexels-anna-nekrashevich-6204571.jpg",
         "name": "Prato Fino",
         "price": 90,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/jar/pexels-marta-dzedyshko-3302499.jpg",
@@ -27,13 +27,13 @@ const db = [
         "source": "./img/products/jar/tom-crew-oiZAQvxTcYQ-unsplash.jpg",
         "name": "Pote Serenata",
         "price": 65.5,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/jar/tom-crew-U9gXnSGSB3w-unsplash.jpg",
         "name": "Jarra Elegância",
         "price": 80.1,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/mug/pexels-agc-15145873.jpg",
@@ -45,25 +45,25 @@ const db = [
         "source": "./img/products/mug/pexels-anastasia-piganova-13946943.jpg",
         "name": "Caneca Elegância",
         "price": 35.4,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/mug/pexels-bimbim-sindu-13649027.jpg",
         "name": "Xícara Paz",
         "price": 25,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/mug/pexels-ivan-samkov-4491277.jpg",
         "name": "Caneca Mukin",
         "price": 37,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/mug/pexels-marta-dzedyshko-15029069.jpg",
         "name": "Xícara Linha",
         "price": 22,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/mug/pexels-sergey-meshkov-8480692.jpg",
@@ -75,13 +75,13 @@ const db = [
         "source": "./img/products/mug/pexels-yan-krukau-6611421.jpg",
         "name": "Caneca Rústica",
         "price": 30,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/set/pexels-andre-william-3010771.jpg",
         "name": "Conjunto Paz",
         "price": 77.7,
-        "home-page": false
+        "home-page": true
     },
     {
         "source": "./img/products/set/pexels-karina-zhukovskaya-7260337.jpg",
